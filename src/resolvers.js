54bf5c70cc4bac7231f2818c3bbe44c80cc4ec23
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-05-09 15:12:20 +0530

    Add files via upload

---
+++ src/resolvers.js
@@ -0,0 +1,8 @@
+export const resolvers = {
+    Query: {
+        hello : () => 'hello'
+    }
+
+}
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
---
+++ src/typeDefs.js
@@ -0,0 +1,10 @@
+import { gql } from "apollo-server-express";
+
+export const typeDefs = gql
+`
+type Query{
+    hello : String!
+
+}
+
+`
