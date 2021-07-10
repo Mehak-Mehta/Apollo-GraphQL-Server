6024ceb07887e9e4b0321c6dd9aa57ecca0c9207
Mehak <76558546+Mehak-Mehta@users.noreply.github.com>
2021-07-10 20:46:59 +0530

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -15,8 +15,8 @@ const startServer  = async() => {
 
     server.applyMiddleware({ app });
 
-     await mongoose.connect('mongodb://localhost:27017/test3', {useNewUrlParser: true, useUnifiedTopology: true});
-
+    await mongoose.connect('mongodb://localhost:27017/test3', {useNewUrlParser: true, useUnifiedTopology: true});
+    
     app.listen({ port: 4000 } ,() => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`))
 
 }
